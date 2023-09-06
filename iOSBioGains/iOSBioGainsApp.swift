6ff654ea9401cdc3b1326f8b04ae25cc0aa186df
Alvin Escobar <alvincsproductions@gmail.com>
2023-09-05 21:32:27 -0500

    Initial Commit

---
+++ iOSBioGains/iOSBioGainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSBioGainsApp.swift
+//  iOSBioGains
+//
+//  Created by Alvin Escobar on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSBioGainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
