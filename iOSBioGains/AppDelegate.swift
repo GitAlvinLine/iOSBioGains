53fe16e5e13697eeefbc5b25226a1cf6fe85c663
Alvin Escobar <alvincsproductions@gmail.com>
2023-09-10 21:59:38 -0500

    Create the 'AppDelegate' file and class that conforms to 'UIResponder', 'UIApplicationDelegate' protocols

---
+++ iOSBioGains/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  iOSBioGains
+//
+//  Created by Alvin Escobar on 9/10/23.
+//
+
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
