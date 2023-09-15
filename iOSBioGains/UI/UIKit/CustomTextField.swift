80d3fff3517448f3b60e18622fb388a0f9a513d5
Alvin Escobar <alvincsproductions@gmail.com>
2023-09-14 21:50:36 -0500

    Create a Preview Provider to view changes of custom view

--- iOSBioGains/UI/UIKit/CustomTextField.swift
+++ iOSBioGains/UI/UIKit/CustomTextField.swift
@@ -6,10 +6,29 @@
 //
 
 import UIKit
+import SwiftUI
 
 class CustomTextField: UITextField {
     convenience init(placeholder: String) {
         self.init()
         self.placeholder = placeholder
+    }
+}
+
+struct CustomTextFieldView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> CustomTextField {
+        return CustomTextField(placeholder: "Email")
+    }
+    
+    func updateUIView(_ uiView: CustomTextField, context: Context) {
+        
+    }
+    
+}
+
+struct CustomTextFieldPreview: PreviewProvider {
+    static var previews: some View {
+        CustomTextFieldView()
     }
 }
