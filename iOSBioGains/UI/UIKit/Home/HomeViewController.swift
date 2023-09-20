7dfb32254884e451189c4174409ca57ad5462c55
Alvin Escobar <alvincsproductions@gmail.com>
2023-09-19 22:18:28 -0500

    Create 'HomeViewController' file

---
+++ iOSBioGains/UI/UIKit/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  iOSBioGains
+//
+//  Created by Alvin Escobar on 9/19/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
